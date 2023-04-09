c6980326106fddc05d03d8d60d9853b43c1551c5
gracexu24 <114009551+gracexu24@users.noreply.github.com>
2023-04-08 18:28:56 -0700

    yea

--- js/scripts.js
+++ js/scripts.js
@@ -22,16 +22,15 @@ window.addEventListener('DOMContentLoaded', event => {
         if (!navbarCollapsible) {
             return;
         }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
+
             navbarCollapsible.classList.add('navbar-shrink')
-        }
+
 
     };
 
     // Shrink the navbar 
     navbarShrink();
+
 
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
